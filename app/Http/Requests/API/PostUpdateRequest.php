55a36184baa6010a3b18609e122bd18b22776d76
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-04-09 15:40:13 +0200

    adding post form request

---
+++ app/Http/Requests/API/PostUpdateRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\API;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "title" => "required|string|max:255",
+            "content" => "required|string",
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'post title is required',
+            'title.max' => 'post title may not be greater than 255 characters',
+            'content.required' => 'post content is required',
+        ];
+    }
+}
